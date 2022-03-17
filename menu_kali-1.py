d0dd400fc34ecd231eafd4cf3c07de8d6a8c4abc
giacomo <ariannagiacomo@gmail.com>
2022-03-17 16:46:09 +0100

    reformatting file

--- menu_kali-1.py
+++ menu_kali-1.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 from subprocess import *
 
 os.environ["SDL_FBDEV"] = "/dev/fb1"
-os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+os.environ["SDL_MOUSEDEV"] = "/dev/input/event9"
 os.environ["SDL_MOUSEDRV"] = "TSLIB"
 
 # Initialize pygame modules individually (to avoid ALSA errors) and hide mouse
